d752da102f27199be62dd60dc45023fb37898d89
tomaspendao <39770209+tomaspendao@users.noreply.github.com>
2021-04-24 09:17:19 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
+var logger = require('morgan');
 var bodyParser = require('body-parser');
 
 var urlencodedParser = bodyParser.urlencoded({extended: false});
@@ -10,6 +11,7 @@ var urlencodedParser = bodyParser.urlencoded({extended: false});
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var criarRouter = require('./routes/criar');
 
 var app = express();
 
@@ -26,6 +28,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/criar', criarRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
