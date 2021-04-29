d3ef71e55f666ffd919e1cd1298cbb944ad8b3c0
tomaspendao <39770209+tomaspendao@users.noreply.github.com>
2021-04-29 20:09:57 +0100

    Add files via upload

--- models/promoter.js
+++ models/promoter.js
@@ -3,7 +3,8 @@ const db = require("../db");
 
 var promoterSchema = new mongoose.Schema({
   name: { type: String, required: true },
-  email: { type: String, required: true },
+  email: { type: String, required: true, unique: true},
+  password: {type: String, required: true},
 });
 
 module.exports = db.model("Promoter", promoterSchema);
