b9a357f4342bf352740b512f26f159996e5bdc7d
tomaspendao <39770209+tomaspendao@users.noreply.github.com>
2021-05-01 13:09:41 +0100

    Delete models directory

--- models/location.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require("mongoose");
-const db = require("../db");
-
-var locationSchema = new mongoose.Schema({
-  place: { type: String },
-  max: { type: Number },
-  limit: { type: Number },
-});
-module.exports = db.model("Location", locationSchema);
